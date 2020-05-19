39401886a80be695ca4eed027cc0d7d25f184aec
zeroproduct <danrica92@yahoo.com>
2020-05-19 15:54:21 -0700

    Initial app commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import niceColors from 'nice-color-palettes/1000';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
 
 function App() {
+  const [randomColorIndex, setRandomColorIndex] = useState(null);
+  const [randomColors, setRandomColors] = useState(null);
+  const [shareButtonText, setShareButtonText] = useState('Share');
+
+  useEffect(() => {
+    let paletteIndex = getURLParams();
+    if (paletteIndex) {
+      setRandomColorIndex(paletteIndex);
+      setRandomColors(niceColors[paletteIndex]);
+    } else {
+      randomize();
+    }
+  }, []);
+
+  const getURLParams = () => {
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    const palette = params.get('palette');
+    return palette;
+  };
+
+  const randomize = () => {
+    let index = Math.floor(Math.random() * niceColors.length);
+    setRandomColorIndex(index);
+    setRandomColors(niceColors[index]);
+  };
+
+  const handleShareClick = () => {
+    setShareButtonText('Link copied!');
+    setTimeout(() => {
+      setShareButtonText('Share');
+    }, 2400);
+  };
+
+  const renderRandomColors = () => {
+    return randomColors.map(color => {
+      return (
+        <CopyToClipboard key={color} text={color}>
+          <div className="palette-color" style={{ backgroundColor: color }}>
+            <div className="hex-container">
+              <span className="hex">{color}<span className="hex-tooltip">Copy</span></span>
+            </div>
+          </div>
+        </CopyToClipboard>
+      );
+    })
+  };
+
+  if (!randomColors) return <div />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {renderRandomColors()}
+      <div className="toolbar">
+        <span className="toolbar-title" role="img" aria-label="palette-pls">🎨🤲</span>
+        <div>
+          <button className="toolbar-button" onClick={() => randomize()}>Randomize</button>
+          <CopyToClipboard 
+            text={
+              window.location.href.includes('palette') 
+                ? `${window.location.href.split('/?')[0]}/?palette=${randomColorIndex}`
+                : `${window.location.href}?palette=${randomColorIndex}`
+            }
+          >
+            <button onClick={() => handleShareClick()} className="toolbar-button">{shareButtonText}</button>
+          </CopyToClipboard>
+        </div>
+      </div>
     </div>
   );
 }
